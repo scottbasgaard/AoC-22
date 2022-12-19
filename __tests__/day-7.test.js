6f93ecb22e0114a05b814ed354bfdfa3dd1c1305
Scott Basgaard <sbasgaard@gmail.com>
2022-12-19 02:32:48 +0100

    feat: day 7

--- __tests__/day-7.test.js
+++ __tests__/day-7.test.js
@@ -2,18 +2,18 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 import parse from '../src/utils/parse.js';
-import { solve as solve1 } from '../src/day-6/part-1.js';
-import { solve as solve2 } from '../src/day-6/part-2.js';
+import { solve as solve1 } from '../src/day-7/part-1.js';
+import { solve as solve2 } from '../src/day-7/part-2.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const FILEPATH = path.resolve(__dirname, './data/day-6.txt');
+const FILEPATH = path.resolve(__dirname, './data/day-7.txt');
 
-describe('day 6', () => {
+describe('day 7', () => {
     describe('part 1', () => {
-        it('returns correct number of characters', () => {
-            const expected = 7;
+        it('returns correct total size', () => {
+            const expected = 95437;
 
             return parse(FILEPATH).then((data) => {
                 const result = solve1(data);
@@ -21,11 +21,9 @@ describe('day 6', () => {
                 expect(result).toBe(expected);
             });
         });
-    });
 
-    describe('part 2', () => {
-        it('returns correct number of characters', () => {
-            const expected = 19;
+        it('returns correct total size', () => {
+            const expected = 24933642;
 
             return parse(FILEPATH).then((data) => {
                 const result = solve2(data);

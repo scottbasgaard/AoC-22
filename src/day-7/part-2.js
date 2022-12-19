fa46391cf38092ba0324aad7fecbff11b2f49779
Scott Basgaard <sbasgaard@gmail.com>
2022-12-19 02:32:48 +0100

    feat: day 7

---
+++ src/day-7/part-2.js
@@ -0,0 +1,109 @@
+import parse from '../utils/parse.js';
+import timer from '../utils/timer.js';
+
+console.time('Time');
+
+const FILEPATH = './src/day-7/data/input.txt';
+const DISK_SPACE = 70000000;
+const REQUIRED_SPACE = 30000000;
+
+export const solve = (lines) => {
+    const filesystem = new Map();
+    const pwd = [];
+
+    const runCommand = (_command) => {
+        const [command, arg] = _command; // eslint-disable-line no-unused-vars
+
+        if (command === 'cd') {
+            if (arg === '/') {
+                pwd.push('root');
+                filesystem.set('root', 0);
+            } else if (arg !== '..') {
+                pwd.push(arg);
+            } else if (arg === '..') {
+                pwd.pop();
+            }
+        }
+    };
+
+    const createDir = (dirname) => {
+        const key = `${pwd.join('/')}/${dirname}`;
+
+        filesystem.set(key, 0);
+    };
+
+    const updateSize = (key, size) => {
+        filesystem.set(key, size);
+    };
+
+    const updateParents = (key, size) => {
+        const [parent, ...rest] = key.split('/').reverse();
+
+        if (parent === 'root') {
+            return;
+        }
+
+        const parentKey = rest.reverse().join('/');
+        const currentSize = filesystem.get(parentKey);
+        const newSize = currentSize + size;
+
+        updateSize(parentKey, newSize);
+        updateParents(parentKey, size);
+    };
+
+    const createFile = (size) => {
+        const key = pwd.join('/');
+
+        const currentSize = filesystem.get(key);
+        const newSize = currentSize + size;
+
+        updateSize(key, newSize);
+        updateParents(key, size);
+    };
+
+    lines.forEach((line) => {
+        const isInput = /^[$]/.test(line);
+        if (isInput) {
+            const terminal = line.split(/\s+/);
+            if (terminal[0] === '$') {
+                const [first, ...rest] = terminal; // eslint-disable-line no-unused-vars
+
+                runCommand(rest);
+            }
+        }
+
+        const dir = /^dir/.test(line);
+        if (dir) {
+            const [_, dirname] = line.split(/\s+/); // eslint-disable-line no-unused-vars
+
+            createDir(dirname);
+        }
+
+        const file = /^\d/.test(line);
+        if (file) {
+            const [size] = line.split(/\s+/);
+
+            createFile(parseInt(size, 10));
+        }
+    });
+
+    const freeSpace = DISK_SPACE - filesystem.get('root');
+    const freeUp = REQUIRED_SPACE - freeSpace;
+
+    const directories = [...filesystem.entries()]
+        .filter(([key, size]) => key !== 'root' && size >= freeUp)
+        .map(([key, size]) => size) // eslint-disable-line no-unused-vars
+        .sort((a, b) => a - b);
+
+    return directories[0];
+};
+
+parse(FILEPATH)
+    .then(solve)
+    .then(timer)
+    .then((result) => console.log(JSON.stringify(result, null, 2)))
+    .catch((err) => {
+        console.error(err);
+    });
+
+export default solve;
